fa7db4a5aaa96feffc257266bfe8ef4c5238b89e
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-05-11 08:59:13 -0300

    Update bhaskara.js

--- Lógica Computacional/Javascript/bhaskara.js
+++ Lógica Computacional/Javascript/bhaskara.js
@@ -1,8 +1,8 @@
-function bhaskara(a, b, c){
+function bhaskara(){
 
-a = prompt ("Digite o valor de A:\n");
-b = prompt ("Digite o valor de B:\n");
-c = prompt  ("Digite o valor de C:\n");
+let a = prompt ("Digite o valor de A:\n");
+let b = prompt ("Digite o valor de B:\n");
+let c = prompt  ("Digite o valor de C:\n");
 
 let delta = b**2-4*a*c
 
