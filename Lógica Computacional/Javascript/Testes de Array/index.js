d7729fb611fea5e33f498614c002146d74695f90
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-05-12 12:09:31 -0300

    Add files via upload

---
+++ Lógica Computacional/Javascript/Testes de Array/index.js
@@ -0,0 +1,18 @@
+function arrays() {
+
+    var soares = []
+    var posicao = soares.indexOf("Selma")
+    soares.push("Vinícius")
+    soares.push("Selma")
+    soares.push("Reinaldo")
+    alert(soares)
+
+    soares[2] = "Anderson"
+    alert(soares)
+
+    soares.splice(posicao, 1)
+    alert(soares)
+
+}
+
+escola()
