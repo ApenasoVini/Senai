fbb1e24661a901cffd33f77ab1b3210421c19b97
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-05-26 15:02:42 -0300

    Atualizações do novo projeto

--- Lógica Computacional/Javascript/Futebol em Array/index.js
+++ Lógica Computacional/Javascript/Futebol em Array/index.js
@@ -1,19 +1,35 @@
 function campo() {
     
-    var goleiro = prompt("Digite o nome do goleiro")
-    var zagueiro1 = prompt("Digite o nome do primeiro zagueiro")
-    var zagueiro2 = prompt("Digite o nome do segundo zagueiro")
-    var latdireito = prompt("Digite o nome do lateral direito")
-    var latesquerdo = prompt("Digite o nome do lateral esquerdo")
-    var volante = prompt("Digite o nome do volante")
-    var meia1 = prompt("Digite o nome do primeiro meia")
-    var meia2 = prompt("Digite o nome do segundo meia")
-    var pontaE = prompt("Digite o nome do ponta esquerda")
-    var pontaD = prompt("Digite o nome do ponta direita")
-    var centroAvante = prompt("Digite o nome do centroavante")
-    
-    var escalacao = [goleiro, +"\n"+ zagueiro1, +"\n"+ zagueiro2, +"\n"+ latdireito, +"\n"+ latesquerdo, +"\n"+ volante, +"\n"+ meia1, +"\n"+ meia2, +"\n"+ pontaD, +"\n"+ pontaE, +"\n"+ centroAvante]
-    
-    time.innerHTML = escalacao
+    var goleiro, zagueiro1, zagueiro2, latdireito, latesquerdo, volante, meia1, meia2, pontaD, pontaE, centroAvante
 
+    do {
+        goleiro = prompt("Digite o nome do goleiro");
+        zagueiro1 = prompt("Digite o nome do primeiro zagueiro");
+        zagueiro2 = prompt("Digite o nome do segundo zagueiro");
+        latdireito = prompt("Digite o nome do lateral direito");
+        latesquerdo = prompt("Digite o nome do lateral esquerdo");
+        volante = prompt("Digite o nome do volante");
+        meia1 = prompt("Digite o nome do primeiro meia");
+        meia2 = prompt("Digite o nome do segundo meia");
+        pontaE = prompt("Digite o nome do ponta esquerda");
+        pontaD = prompt("Digite o nome do ponta direita");
+        centroAvante = prompt("Digite o nome do centroavante");
+    } while (
+        typeof goleiro == null ||
+        typeof zagueiro1 == null ||
+        typeof zagueiro2 == null ||
+        typeof latdireito == null ||
+        typeof latesquerdo == null ||
+        typeof volante == null ||
+        typeof meia1 == null ||
+        typeof meia2 == null ||
+        typeof pontaD == null ||
+        typeof pontaE == null ||
+        typeof centroAvante == null
+    );
+
+    var escalacao = [goleiro, zagueiro1, zagueiro2, latdireito, latesquerdo, volante, meia1, meia2, pontaD, pontaE, centroAvante];
+
+    var timeElement = document.getElementById('time');
+    timeElement.innerHTML = escalacao.join('<br>');
 }
