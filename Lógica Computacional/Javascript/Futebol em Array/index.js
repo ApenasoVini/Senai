f016be77e9cad79def83dcec28dc7824f48713b8
ApenasoVini <viniciusicmsc@gmail.com>
2023-05-26 11:39:54 -0300

    Atualizações de Criação de um novo projeto

---
+++ Lógica Computacional/Javascript/Futebol em Array/index.js
@@ -0,0 +1,19 @@
+function campo() {
+    var goleiro = prompt("Digite o nome do goleiro")
+    var zagueiro1 = prompt("Digite o nome do primeiro zagueiro")
+    var zagueiro2 = prompt("Digite o nome do segundo zagueiro")
+    var latdireito = prompt("Digite o nome do lateral direito")
+    var latesquerdo = prompt("Digite o nome do lateral esquerdo")
+    var volante = prompt("Digite o nome do volante")
+    var meia1 = prompt("Digite o nome do primeiro meia")
+    var meia2 = prompt("Digite o nome do segundo meia")
+    var pontaE = prompt("Digite o nome do ponta esquerda")
+    var pontaD = prompt("Digite o nome do ponta direita")
+    var centroAvante = prompt("Digite o nome do centroavante")
+
+    var time = [goleiro, zagueiro1, zagueiro2, latdireito, latesquerdo, volante, meia1, meia2, pontaD, pontaE, centroAvante]
+    return time
+
+}
+
+campo()
