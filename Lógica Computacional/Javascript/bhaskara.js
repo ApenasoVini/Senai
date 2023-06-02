196e9d82e70e853b9d21e70bba06a4407120c377
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-06-02 11:14:22 -0300

    Atualizações

--- Lógica Computacional/Javascript/bhaskara.js
+++ Lógica Computacional/Javascript/bhaskara.js
@@ -1,25 +1,25 @@
-function bhaskara(){
+function bhaskara() {
 
-let a = prompt ("Digite o valor de A:\n");
-let b = prompt ("Digite o valor de B:\n");
-let c = prompt  ("Digite o valor de C:\n");
+    let a = prompt("Digite o valor de A:\n");
+    let b = prompt("Digite o valor de B:\n");
+    let c = prompt("Digite o valor de C:\n");
 
-let delta = b**2-4*a*c
+    let delta = b ** 2 - 4 * a * c
 
-if (delta < 0){
-   alert("Não existem raízes reais")
-}
-else if (delta == 0){
-    let xunico = (-b + Math.sqrt(delta)) / (2 * a);
-    alert("A única raíz é "+ xunico)
-}
-else {
+    if (delta < 0) {
+        alert("Não existem raízes reais")
+    }
+    else if (delta == 0) {
+        let xUnico = (-b + Math.sqrt(delta)) / (2 * a);
+        alert("A única raíz é " + xUnico)
+    }
+    else {
 
-let x1 = (-b + Math.sqrt(delta)) / (2 * a);
-let x2 = (-b + Math.sqrt(delta)) / (2 * a);
+        let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        let x2 = (-b + Math.sqrt(delta)) / (2 * a);
 
-alert("Seus resultados são: "+ x1+" e "+ x2)
-}
+        alert("Seus resultados são: " + x1 + " e " + x2)
+    }
 }
 
 bhaskara()
