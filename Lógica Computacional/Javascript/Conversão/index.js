d20f9887c7c8cdbfd29f8ee840fd75d49332a923
Vini <vi058587@intelbras.com.br>
2023-06-09 14:26:51 -0300

    Atualizações de lógica em JS

--- Lógica Computacional/Javascript/Conversão/index.js
+++ Lógica Computacional/Javascript/Conversão/index.js
@@ -1,27 +1,27 @@
 function converte() {
 
-    var temperatura = Number(document.getElementById("temperatura").value)
+    var temperatura = Number(document.getElementById("temperatura").value);
 
-    var resultadoFinal = 0
+    var resultadoFinal = 0;
 
     if (document.getElementById("cf").checked) {
-        resultadoFinal = temperatura * (9 / 5) + 32
+        resultadoFinal = temperatura * (9 / 5) + 32;
     }
     else if (document.getElementById("ck").checked) {
-        resultadoFinal = temperatura + 273.15
+        resultadoFinal = temperatura + 273.15;
     }
     else if (document.getElementById("fc").checked) {
-        resultadoFinal = (temperatura - 32) * (5 / 9)
+        resultadoFinal = (temperatura - 32) * (5 / 9);
     }
     else if (document.getElementById("fk").checked) {
-        resultadoFinal = (temperatura - 32) * (5 / 9) + 273.15
+        resultadoFinal = (temperatura - 32) * (5 / 9) + 273.15;
     }
     else if (document.getElementById("kc").checked) {
-        resultadoFinal = temperatura - 273.15
+        resultadoFinal = temperatura - 273.15;
     }
     else if (document.getElementById("kf").checked) {
-        resultadoFinal = (temperatura - 273.15) * (9 / 5) + 32
+        resultadoFinal = (temperatura - 273.15) * (9 / 5) + 32;
     }
 
-    document.getElementById("resultado").innerHTML = resultadoFinal
+    document.getElementById("resultado").innerHTML = resultadoFinal;
 }
