ba57bf50ef91dc4964eaa3ca9388ee2e1c881124
Vini <vi058587@intelbras.com.br>
2023-06-15 15:47:07 -0300

    Atualizações de DB

--- Banco de Dados/SQL/diagramaescola.sql
+++ Banco de Dados/SQL/diagramaescola.sql
@@ -8,13 +8,13 @@ PRIMARY KEY(cpf_pk)
 );
 
 CREATE TABLE escola (
-    nome VARCHAR(50) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    cnpj_pk VARCHAR(20) NOT NULL,
-    grau_escolaridade VARCHAR(20) NOT NULL,
-    cpf_fk VARCHAR(15) NOT NULL,
-    PRIMARY KEY(cnpj_pk),
-    FOREIGN KEY(cpf_fk) REFERENCES aluno (cpf_pk) 
+nome VARCHAR(50) NOT NULL,
+endereco VARCHAR(100) NOT NULL,
+cnpj_pk VARCHAR(20) NOT NULL,
+grau_escolaridade VARCHAR(20) NOT NULL,
+cpf_fk VARCHAR(15) NOT NULL,
+PRIMARY KEY(cnpj_pk),
+FOREIGN KEY(cpf_fk) REFERENCES aluno (cpf_pk) 
 );
 
 INSERT INTO aluno (nome, idade, turma, itinerario, cpf_pk) 
