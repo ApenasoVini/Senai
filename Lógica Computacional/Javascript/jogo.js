4e21d6289a2f193eb7ad45b809fe9c6f0eee0b07
vi058587 <vinicius.franca@intelbras.com>
2023-09-26 16:35:44 -0300

    Lógica

---
+++ Lógica Computacional/Javascript/jogo.js
@@ -0,0 +1,22 @@
+function pedra_papel_tesoura() {
+    do {
+        var user = prompt('Pedra, papel ou tesoura? ').toLowerCase();
+    } while (user !== 'pedra' && user !== 'papel' && user !== 'tesoura');
+
+    var num = Math.floor(Math.random() * 3) + 1;
+    var opcoes = ['pedra', 'papel', 'tesoura'];
+    var ia = opcoes[num - 1];
+
+    if (ia === user) {
+        alert('Empate!');
+    } else if (
+        (ia === 'pedra' && user === 'tesoura') ||
+        (ia === 'papel' && user === 'pedra') ||
+        (ia === 'tesoura' && user === 'papel')
+    ) {
+        alert('IA ganhou!');
+    } else {
+        alert('Você ganhou!');
+    }
+}
+pedra_papel_tesoura();
--- Lógica Computacional/Javascript/media.js
+++ Lógica Computacional/Javascript/media.js
@@ -1,11 +1,11 @@
 function media() {
     do {
-        var n = prompt("Quantos números você quer usar na média?");
+        var n = prompt(`Quantos números você quer usar na média?`);
     } while (n <= 0);
     var resultado, soma = 0
 
     for (var i = 1; i <= n; i++) {
-        soma += Number(prompt("Digite o " + i + "º número:"));
+        soma += Number(prompt(`Digite o ${i}º número:`));
     }
     resultado = soma / n
     alert(`A média é ${resultado}`);
