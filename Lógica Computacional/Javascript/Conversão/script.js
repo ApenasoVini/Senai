a77017f3a433c2f61aabd5224d4894014d7ad4b8
Vini <viniciusicmsc@gmail.com>
2023-09-27 16:06:34 -0300

    PRONTO

--- Lógica Computacional/Javascript/Conversão/script.js
+++ Lógica Computacional/Javascript/Conversão/script.js
@@ -1,27 +1,40 @@
 function converte() {
+    var temperatura = Number(document.getElementById("number").value);
+    var tipo = document.getElementById("tipo").value;
+    var resultado = 0;
 
-    var temperatura = Number(document.getElementById("temperatura").value);
-
-    var resultadoFinal = 0;
-
-    if (document.getElementById("cf").checked) {
-        resultadoFinal = temperatura * (9 / 5) + 32;
+    if (tipo == "Celsius") {
+        if (document.getElementById("f").checked) {
+            resultado = temperatura * (9 / 5) + 32;
+        }
+        else if (document.getElementById("k").checked) {
+            resultado = temperatura + 273.15;
+        }
+        else {
+            resultado = temperatura;
+        }
     }
-    else if (document.getElementById("ck").checked) {
-        resultadoFinal = temperatura + 273.15;
+    else if (tipo == "Fahrenheit") {
+        if (document.getElementById("c").checked) {
+            resultado = (temperatura - 32) * (5 / 9);
+        }
+        else if (document.getElementById("k").checked) {
+            resultado = (temperatura - 32) * (5 / 9) + 273.15;
+        }
+        else {
+            resultado = temperatura;
+        }
     }
-    else if (document.getElementById("fc").checked) {
-        resultadoFinal = (temperatura - 32) * (5 / 9);
+    else {
+        if (document.getElementById("c").checked) {
+            resultado = temperatura - 273.15;
+        }
+        else if (document.getElementById("f").checked) {
+            resultado = (temperatura - 273.15) * (9 / 5) + 32;
+        }
+        else {
+            resultado = temperatura;
+        }
     }
-    else if (document.getElementById("fk").checked) {
-        resultadoFinal = (temperatura - 32) * (5 / 9) + 273.15;
-    }
-    else if (document.getElementById("kc").checked) {
-        resultadoFinal = temperatura - 273.15;
-    }
-    else if (document.getElementById("kf").checked) {
-        resultadoFinal = (temperatura - 273.15) * (9 / 5) + 32;
-    }
-
-    document.getElementById('res').innerHTML = resultadoFinal;
+    document.getElementById("res").value = resultado;
 }
