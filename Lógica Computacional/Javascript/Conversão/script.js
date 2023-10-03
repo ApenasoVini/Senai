1a4f2a56d607e2d9845d91417b25902202495a1f
vi058587 <vinicius.franca@intelbras.com>
2023-10-03 16:52:23 -0300

    CSS

--- Lógica Computacional/Javascript/Conversão/script.js
+++ Lógica Computacional/Javascript/Conversão/script.js
@@ -2,39 +2,49 @@ function converte() {
     var temperatura = Number(document.getElementById("number").value);
     var tipo = document.getElementById("tipo").value;
     var resultado = 0;
+    var resultadoA = 0;
 
     if (tipo == "Celsius") {
         if (document.getElementById("f").checked) {
             resultado = temperatura * (9 / 5) + 32;
+            resultadoA = resultado.toFixed(2)
         }
         else if (document.getElementById("k").checked) {
             resultado = temperatura + 273.15;
+            resultadoA = resultado.toFixed(2)
         }
         else {
             resultado = temperatura;
+            resultadoA = resultado.toFixed(2)
         }
     }
     else if (tipo == "Fahrenheit") {
         if (document.getElementById("c").checked) {
             resultado = (temperatura - 32) * (5 / 9);
+            resultadoA = resultado.toFixed(2)
         }
         else if (document.getElementById("k").checked) {
             resultado = (temperatura - 32) * (5 / 9) + 273.15;
+            resultadoA = resultado.toFixed(2)
         }
         else {
             resultado = temperatura;
+            resultadoA = resultado.toFixed(2)
         }
     }
     else {
         if (document.getElementById("c").checked) {
             resultado = temperatura - 273.15;
+            resultadoA = resultado.toFixed(2)
         }
         else if (document.getElementById("f").checked) {
             resultado = (temperatura - 273.15) * (9 / 5) + 32;
+            resultadoA = resultado.toFixed(2)
         }
         else {
             resultado = temperatura;
+            resultadoA = resultado.toFixed(2)
         }
     }
-    document.getElementById("res").value = resultado;
+    document.getElementById("res").value = resultadoA;
 }
