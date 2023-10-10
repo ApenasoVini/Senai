780f261b8bbfbd5d556c738fc8e8192d78c2e63c
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-10-10 08:46:13 -0300

    CSS

--- Lógica Computacional/Javascript/Conversão/script.js
+++ Lógica Computacional/Javascript/Conversão/script.js
@@ -2,49 +2,39 @@ function converte() {
     var temperatura = Number(document.getElementById("number").value);
     var tipo = document.getElementById("tipo").value;
     var resultado = 0;
-    var resultadoA = 0;
 
     if (tipo == "Celsius") {
         if (document.getElementById("f").checked) {
             resultado = temperatura * (9 / 5) + 32;
-            resultadoA = resultado.toFixed(2)
         }
         else if (document.getElementById("k").checked) {
             resultado = temperatura + 273.15;
-            resultadoA = resultado.toFixed(2)
         }
         else {
             resultado = temperatura;
-            resultadoA = resultado.toFixed(2)
         }
     }
     else if (tipo == "Fahrenheit") {
         if (document.getElementById("c").checked) {
             resultado = (temperatura - 32) * (5 / 9);
-            resultadoA = resultado.toFixed(2)
         }
         else if (document.getElementById("k").checked) {
             resultado = (temperatura - 32) * (5 / 9) + 273.15;
-            resultadoA = resultado.toFixed(2)
         }
         else {
             resultado = temperatura;
-            resultadoA = resultado.toFixed(2)
         }
     }
     else {
         if (document.getElementById("c").checked) {
             resultado = temperatura - 273.15;
-            resultadoA = resultado.toFixed(2)
         }
         else if (document.getElementById("f").checked) {
             resultado = (temperatura - 273.15) * (9 / 5) + 32;
-            resultadoA = resultado.toFixed(2)
         }
         else {
             resultado = temperatura;
-            resultadoA = resultado.toFixed(2)
         }
     }
-    document.getElementById("res").value = resultadoA;
+    document.getElementById("res").value = resultado;
 }
