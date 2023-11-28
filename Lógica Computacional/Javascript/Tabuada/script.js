1e260d278b961810d32627f0e08c2d10b9273f9a
vini <viniciusicmsc@gmail.com>
2023-11-28 09:39:51 -0300

    Fim

--- Lógica Computacional/Javascript/Tabuada/script.js
+++ Lógica Computacional/Javascript/Tabuada/script.js
@@ -1,12 +1,19 @@
 function cores() {
-    var one = parseInt(document.getElementById("inputNumberOne").value);
-    var two = parseInt(document.getElementById("inputNumberTwo").value);
-    var resposta = one * two;
-    var valorFinal = document.getElementById("valorFinal")
+    var resposta = parseInt(document.getElementById("inputNumberOne").value) * parseInt(document.getElementById("inputNumberTwo").value);
+    var valorFinal = document.getElementById("valorFinal");
     valorFinal.value = resposta;
-    var divRes = document.getElementById("res");
+    var cx1 = document.getElementById("cx1");
+    var cx2 = document.getElementById("cx2");
+    var cx3 = document.getElementById("cx3");
+    var cx4 = document.getElementById("cx4");
+    var cx5 = document.getElementById("cx5");
+    cx1.style.backgroundColor = "black";
+    cx2.style.backgroundColor = "black";
+    cx3.style.backgroundColor = "black";
+    cx4.style.backgroundColor = "black";
+    cx5.style.backgroundColor = "black";
     let cores = [4, 6, 11, 7, 9];
-    let validos = []
+    let validos = [];
 
     for (i = 0; i < cores.length; i++) {
         if (resposta % cores[i] === 0) {
@@ -15,36 +22,24 @@ function cores() {
     }
 
     for (x = 0; x < validos.length; x++) {
-        if (x === 4) {
-            var red = document.createElement('div');
-            red.style.width = "20vw";
-            red.style.height = "20vh";
-            red.style.backgroundColor = "red";
-            divRes.appendChild("red");
+        if (validos[x] === 4) {
+            cx1.style.backgroundColor = "red"
         }
-        else if (x === 6) {
-            var green = document.createElement('div');
-            green.style.width = "20%";
-            green.style.backgroundColor = "green"
-            divRes.appendChild("green");
+
+        else if (validos[x] === 6) {
+            cx2.style.backgroundColor = "green"
         }
-        else if (x === 11) {
-            var yellow = document.createElement('div');
-            yellow.style.width = "20%";
-            yellow.style.backgroundColor = "yellow"
-            divRes.appendChild("yellow");
+
+        else if (validos[x] === 11) {
+            cx3.style.backgroundColor = "yellow"
         }
-        else if (x === 7) {
-            var blue = document.createElement('div');
-            blue.style.width = "20%";
-            blue.style.backgroundColor = "blue"
-            divRes.appendChild("blue");
+
+        else if (validos[x] === 7) {
+            cx4.style.backgroundColor = "blue"
         }
+
         else {
-            var purple = document.createElement('div');
-            purple.style.width = "20%";
-            purple.style.backgroundColor = "purple"
-            divRes.appendChild("purple");
+            cx5.style.backgroundColor = "purple"
         }
     }
 
